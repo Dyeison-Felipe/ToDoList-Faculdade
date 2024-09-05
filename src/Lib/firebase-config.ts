1b1f10d96d300322380f96ca509bc49114c42161
NatanTeixeiraVieira <natanteixeiravieira@gmail.com>
2024-09-05 00:59:00 -0300

    chore: Add .env file to firebase configs

--- src/Lib/firebase-config.ts
+++ src/Lib/firebase-config.ts
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBgJdHIUwBZGHCrXaB6lYonIGHpK20Fr4A',
-  authDomain: 'todo-15c15.firebaseapp.com',
-  projectId: 'todo-15c15',
-  storageBucket: 'todo-15c15.appspot.com',
-  messagingSenderId: '54589800411',
-  appId: '1:54589800411:web:4177eb6cf377274f3ff1fe',
-  measurementId: 'G-46762TG58V',
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
