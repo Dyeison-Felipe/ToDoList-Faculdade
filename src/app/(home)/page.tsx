cbd16f36183a2bd1ab14237bba246d7a8ca5a01d
Dyeison-Felipe <dyeisonfc@outlook.com>
2024-09-07 09:23:03 -0300

    feat: navbar and home

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Page() {
+  return (
+    <section className="w-full h-screen flex flex-col justify-center items-center text-5xl">
+      Bem vindo ao To-Do-List
+    </section>
+  )
+}
---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,7 @@
+export default function index() {
+  return (
+    <nav>
+      <h1></h1>
+    </nav>
+  )
+}
